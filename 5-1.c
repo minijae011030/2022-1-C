eac9d8fa43a9f6e5981822b528204c114ab92b96
강민재 <kangminjae@gangminjaes-MacBook-Air.local>
2022-07-29 01:25:24 +0900

    정수 N을 입력 받아 다음을 출력하는 프로그램을 작성하시오. - 0도 미만이면 실내 활동이 바람직하므로 cold, indoor 출력  - 0도 이상이면서 40도 미만이면 실외 활동 가능하므로 moderate, outdoor 출력  - 40도 이상이면 실내 활동이 바람직하므로 hot, indoor 출력

---
+++ 5-1.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main() {
+    // 1번
+    int n;
+    scanf("%d", &n);
+    if (n < 0) {
+        printf("cold, indoor");
+    } else if (n >= 0 && n < 40) {
+        printf("moderate, outdoor");
+    } else {
+        printf("hot, indoor");
+    }
+}
