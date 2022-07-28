bd4efc0225df7b8d63b1b2f7373c606315deb7f3
강민재 <kangminjae@gangminjaes-MacBook-Air.local>
2022-07-29 01:51:27 +0900

    hello

--- 5-2.c
+++ 5-2.c
@@ -1,21 +1,10 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 int main() {
-    // 1번
-    int n;
-    scanf("%d", &n);
-    
-    if (n < 0) {
-        printf("cold, indoor");
-    } else if (n >= 0 && n < 40) {
-        printf("moderate, outdoor");
-    } else {
-        printf("hot, indoor");
-    }
-
     // 2번
     int n, m;
     scanf("%d %d", &n, &m);
+
     if (n > m) {
         if (m * m == n){
             printf("%d*%d=%d", m, m, n);
@@ -29,4 +18,5 @@ int main() {
             printf("none");
         }
     }
+    
 }
